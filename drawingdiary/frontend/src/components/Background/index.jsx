4aaf163f640c8003a3b95947e25dff3890d02323
1-yuna <97020423+1-yuna@users.noreply.github.com>
2024-01-14 14:57:20 +0900

    [feat] 배경

---
+++ drawingdiary/frontend/src/components/Background/index.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const BackgroundColor = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(45deg, #f8e5eb, #dadff4);
+  width: 100%;
+  height: 100vh;
+`;
+
+const BackgroundBox = styled.div`
+  width: 1000px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.08);
+  box-shadow: 3px 3px 20px 5px rgba(0, 0, 0, 0.03);
+  border-radius: 20px;
+`;
+
+function Background() {
+  return (
+    <div>
+      <BackgroundColor>
+        <BackgroundBox></BackgroundBox>
+      </BackgroundColor>
+    </div>
+  );
+}
+
+export default Background;
