d61e201bd049589525659930d6f7c71240062b01
jiwon <jiwon0768@tukorea.ac.kr>
2024-02-13 13:37:17 +0900

    수정

---
+++ drawingdiary/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CreateAccount from './pages/CreateAccount';
+import SideBar from './components/sidebar/SideBar';
+
+function App() {
+  return (
+    <div>
+      {/* <SideBar/> */}
+      <CreateAccount />
+    </div>
+  );
+}
+
+export default App;
