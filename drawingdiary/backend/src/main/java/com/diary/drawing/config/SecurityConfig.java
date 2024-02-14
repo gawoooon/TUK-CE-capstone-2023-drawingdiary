ee469d4ba96b00225dcc2b8018d7b0f59a049039
gawon <gawonlee0712@gmail.com>
2024-02-14 21:01:40 +0900

    [수정] cors 설정 테스트 securityconfig 수정

--- drawingdiary/backend/src/main/java/com/diary/drawing/config/SecurityConfig.java
+++ drawingdiary/backend/src/main/java/com/diary/drawing/config/SecurityConfig.java
@@ -1,13 +1,10 @@
 package com.diary.drawing.config;
 
-
-
-
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,6 +13,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.diary.drawing.jwt.security.JwtAuthenticationFilter;
 import com.diary.drawing.jwt.service.CustomUserDetailService;
@@ -23,38 +23,38 @@ import com.diary.drawing.jwt.service.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
-@EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터 체인에 등록
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록
 @RequiredArgsConstructor
-public class SecurityConfig{
-
+public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final CustomUserDetailService customUserDetailService;
 
     @Bean
     public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
-        //내가 만든 토큰 필터 먼저
-		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        // 내가 만든 토큰 필터 먼저
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         http
-                //csrf disable csrf 공격 막아주기
+                // csrf disable csrf 공격 막아주기
                 .csrf(AbstractHttpConfigurer::disable)
                 // cors 설정(react랑) 나중에 Controller에 @CrossOrigin으로 싹 넣어야함
-                .cors(Customizer.withDefaults())
-				//Form 로그인 방식 disable
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // Form 로그인 방식 disable
                 .formLogin((auth) -> auth.disable())
-                //oath2 로그인 방식은 잠깐 disable
+                // oath2 로그인 방식은 잠깐 disable
                 .oauth2Login((auth) -> auth.disable())
-				//http basic 인증 방식 disable
+                // http basic 인증 방식 disable
                 .httpBasic((auth) -> auth.disable())
-				//경로별 인가 작업
+                // 경로별 인가 작업
                 .authorizeHttpRequests((auth) -> auth
                         // 일단 죄다 허가 해놨음
                         .requestMatchers("/login", "/**", "/join", "/api/**",
-                                            "/error", "/auth/**", "/auth/login").permitAll()
-												//.requestMatchers("/admin").hasRole("ADMIN")
+                                "/error", "/auth/**", "/auth/login")
+                        .permitAll()
+                        // .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated())
-                //세션을 생성하지 않고, 요청마다 새로운 인증을 수행하도록 구성하는 옵션으로 REST API와 같은 환경에서 사용
+                // 세션을 생성하지 않고, 요청마다 새로운 인증을 수행하도록 구성하는 옵션으로 REST API와 같은 환경에서 사용
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
@@ -75,6 +75,20 @@ public class SecurityConfig{
                 .passwordEncoder(bCryptPasswordEncoder());
         return builder.build();
     }
+
     // react 서버와 연동하는 cors (나중에 추가)
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://locahost:3000"));    // react 허가
+        configuration.setAllowedMethods(Arrays.asList("*"));    // "GET", "POST", "PUT", "DELETE"
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);    // 일단 허용
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
