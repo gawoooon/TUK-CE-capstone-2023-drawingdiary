edc12d2d51230779d11dbd0c09181deed1ddb422
jiwon <jiwon0768@tukorea.ac.kr>
2024-02-14 21:55:46 +0900

    [지원] cors 계속 수정 중..

--- drawingdiary/backend/server.js
+++ drawingdiary/backend/server.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(json());
 
 // 특정 출처의 요청만 허용
-app.use(cors({
-    origin: '*' // 클라이언트 애플리케이션 주소
-}));
+// app.use(cors({
+//     origin: '*' // 클라이언트 애플리케이션 주소
+// }));
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
