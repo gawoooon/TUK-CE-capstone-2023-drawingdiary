f6a501e9e4918b8345b7844de6438d2f05eeb162
jiwon <jiwon0768@tukorea.ac.kr>
2024-02-21 16:25:33 +0900

    [지원] 감정분석 api 키 수정

--- drawingdiary/backend/src/main/java/com/diary/drawing/sentiment/service/SentimentService.java
+++ drawingdiary/backend/src/main/java/com/diary/drawing/sentiment/service/SentimentService.java
@@ -5,6 +5,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.diary.drawing.sentiment.dto.SentimentDTO;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -12,14 +13,20 @@ import org.springframework.http.MediaType;
 @Service
 public class SentimentService {
 
+    @Value("${naver.api.key.id}")
+    private String apiKeyId;
+
+    @Value("${naver.api.key}")
+    private String apiKey;
+
     public SentimentDTO analyzeSentiment(String text) {
         final String apiUrl = "https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze";
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("X-NCP-APIGW-API-KEY-ID", "ksnv4tfdvd");
-        headers.set("X-NCP-APIGW-API-KEY", "fcVwyGMNR7eJjAPDbMDZxdsOobEl1jHlvsS6RAC8");
+        headers.set("X-NCP-APIGW-API-KEY-ID", apiKeyId);
+        headers.set("X-NCP-APIGW-API-KEY", apiKey);
 
         String requestJson = "{\"content\": \"" + text + "\"}";
 
