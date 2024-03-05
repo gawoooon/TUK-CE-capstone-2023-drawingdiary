20b09831986139970b51d9303de1a055b975250d
jiwon <jiwon0768@tukorea.ac.kr>
2024-03-05 23:13:48 +0900

    [지원] 이메일 인증 수정 1

--- drawingdiary/frontend/src/pages/JoinPage.jsx
+++ drawingdiary/frontend/src/pages/JoinPage.jsx
@@ -83,6 +83,21 @@ const ConfilmPasswordStyle = styled.input`
   font-size: 13px;
 `;
 
+const VerifyButton = styled.button `
+  height: 40px;
+  width: 60px;
+  margin-left: 10px;
+  background-color: rgba(106, 156, 253, 0.3);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  color: black;
+  font-size: 13px;
+  text-align: center;
+  outline: none;
+`;
+
 const ButtonContainer = styled.div `
     display: flex;
     align-items: center;
@@ -190,8 +205,22 @@ const CreateAccount = () => {
             console.log('Error: ', error);
           }
         });
+
     };
 
+    const sendEmail = async () => {
+      if(email !== '') {
+        try {
+          const response = await axios.post('http://localhost:8080/api/email/codesending', { email })
+          console.log("response", response);
+        } catch (error) {
+          console.log("error: ", error);
+        }
+      } else {
+        alert("이메일을 입력해주세요!");
+      }
+    };
+      
     return (
         <Background>
 
@@ -282,14 +311,14 @@ const CreateAccount = () => {
                   onChange={ (e) => setEmail(e.target.value)}
                   placeholder="이메일"/>
                 
-                <SmallButton text="인증"/>
+                <VerifyButton onClick={sendEmail}>인증</VerifyButton>
+
               </InputFieldStyle>
 
               <InputFieldStyle>
 
                 <EmailInputStyle
                   id="certification"
-                  type="email"
                   value={certification}
                   onChange={(e) => setCheckCertification(e.target.value)}
                   placeHolder="인증번호 입력"/>
