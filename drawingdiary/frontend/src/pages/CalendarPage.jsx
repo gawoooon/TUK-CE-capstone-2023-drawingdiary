154e8d29d34cedd9b1517653b7daec8ad37743c3
1-yuna <micha0215@naver.com>
2024-03-28 14:00:26 +0900

    [윤아] 캘린더 무한렌더링 수정 중
    무한 렌더링은 수정됐는데 ui 오류 뜸

--- drawingdiary/frontend/src/pages/CalendarPage.jsx
+++ drawingdiary/frontend/src/pages/CalendarPage.jsx
@@ -11,7 +11,10 @@ import FalseComponent from "../components/FalseComponent";
 import { useAuth } from "../auth/context/AuthContext";
 
 import { GrFormPreviousLink } from "react-icons/gr";
-import { CalendarProvider, useCalendar } from "../components/Calendar2/CalendarProvider";
+import {
+  CalendarProvider,
+  useCalendar,
+} from "../components/Calendar2/CalendarProvider";
 import axios from "axios";
 
 const Body = styled.body`
@@ -121,7 +124,7 @@ const EditBtn = styled.button`
   &:hover {
     background-color: #f9f9f9;
   }
-  `;
+`;
 
 const RemoveBtn = styled.button`
   width: 65px;
@@ -184,8 +187,8 @@ function CalendarPage() {
   const [isOpen, setIsOpen] = useState(true);
 
   const { memberID } = useAuth();
-  const accessToken = localStorage.getItem('accessToken');
-  
+  const accessToken = localStorage.getItem("accessToken");
+
   const { year, month } = useCalendar();
   const [data, setData] = useState([]);
   const [imageUrl, setImageUrl] = useState("");
@@ -195,6 +198,25 @@ function CalendarPage() {
   const [isSelectedMonth, setIsSelectedMonth] = useState("");
   const [isSelectedDay, setIsSelectedDay] = useState("");
 
+  useEffect(() => {
+    fetchCalendar();
+  }, []); // 컴포넌트가 처음으로 렌더링될 때만 실행
+
+  const fetchCalendar = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/calender/${year}-${month}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      setData(response.data);
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
 
   const handleDateClick = async (day) => {
     if (isSameDay(day, selectedDate)) {
@@ -227,12 +249,11 @@ function CalendarPage() {
   };
 
   const handleEdit = async () => {
-
     const currentYear = selectedDate.getFullYear();
     const month = selectedDate.getMonth() + 1;
     const day = selectedDate.getDate();
     const formattedDate = new Date(currentYear, month - 1, day);
-    
+
     // 날짜 및 월을 두 자릿수로 표시하는 함수
     const pad = (number) => (number < 10 ? `0${number}` : number);
 
@@ -242,14 +263,17 @@ function CalendarPage() {
     )}-${pad(formattedDate.getDate())}`;
 
     try {
-      const response = await axios.get(`http://localhost:8080/api/diary/${dateString}`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`
+      const response = await axios.get(
+        `http://localhost:8080/api/diary/${dateString}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
         }
-      });
+      );
       const dataArray = response.data;
       console.log(dataArray);
-  
+
       const diaryText = dataArray.text;
       const weather = dataArray.weather;
       const albumName = dataArray.albumName;
@@ -257,9 +281,20 @@ function CalendarPage() {
       const comment = dataArray.comment;
       const style = dataArray.styleName;
       const sentiment = dataArray.sentiment;
-    
+
       navigate(`/showDiary/${memberID}/${formattedDate}`, {
-        state: { date: { currentYear, month, day }, diaryData: { weather, albumName, diaryText, style, image, comment, sentiment } },
+        state: {
+          date: { currentYear, month, day },
+          diaryData: {
+            weather,
+            albumName,
+            diaryText,
+            style,
+            image,
+            comment,
+            sentiment,
+          },
+        },
       });
     } catch (error) {
       console.log("error: ", error);
@@ -267,57 +302,45 @@ function CalendarPage() {
   };
 
   const handleRemove = async () => {
-
     try {
-      await axios.delete(`http://localhost:8080/api/diary/${isSelectedYear}-${isSelectedMonth}-${isSelectedDay}`, {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`
+      await axios.delete(
+        `http://localhost:8080/api/diary/${isSelectedYear}-${isSelectedMonth}-${isSelectedDay}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
         }
-      })
+      );
       fetchCalendar();
     } catch (error) {
       console.log("error: ", error);
     }
   };
 
-  const fetchCalendar = async () => {
-    try {
-      const response = await axios.get(`http://localhost:8080/api/calender/${year}-${month}`, {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`
-        }
-      })
-      setData(response.data);
-    } catch (error) {
-      console.log("error: ", error);
-    };
-  };
-
   // fetchData 함수를 useEffect 외부에서 선언
-  const fetchData = async (date) => {
-    try {
-      // isSameDay함수를 사용하여 selectedDate와 일치하는 날짜를 찾음
-      const index = data.findIndex((item) =>
-        isSameDay(new Date(item.date), date),
-      );
+  // const fetchData = async (date) => {
+  //   try {
+  //     // isSameDay함수를 사용하여 selectedDate와 일치하는 날짜를 찾음
+  //     const index = data.findIndex((item) =>
+  //       isSameDay(new Date(item.date), date)
+  //     );
 
-      //일치하면 인덱스 값/ 아니면 -1 반환 => 존재하면 true, 존재하지 않으면 false
-      if(index !== -1) {
-        setText(data[index].text);
-        setImageUrl(data[index].imageFile);
-      }
-      const hasData = index !== -1;
-      return hasData;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return false; // 에러가 발생하면 데이터가 없는 것으로 처리
-    }
-  };
+  //     //일치하면 인덱스 값/ 아니면 -1 반환 => 존재하면 true, 존재하지 않으면 false
+  //     if (index !== -1) {
+  //       setText(data[index].text);
+  //       setImageUrl(data[index].imageFile);
+  //     }
+  //     const hasData = index !== -1;
+  //     return hasData;
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //     return false; // 에러가 발생하면 데이터가 없는 것으로 처리
+  //   }
+  // };
 
   // useEffect 내부에서 fetchData 함수 호출(변경 감지)
+  // fetchDataAndUpdateState 함수를 useEffect 외부로 이동
   useEffect(() => {
-    fetchCalendar();
-
     const fetchDataAndUpdateState = async () => {
       if (selectedDate) {
         setIsSelectedYear(format(selectedDate, "yyyy"));
@@ -325,28 +348,35 @@ function CalendarPage() {
         setIsSelectedDay(format(selectedDate, "dd"));
 
         try {
-          // 클릭한 날짜
-          const hasData = await fetchData(selectedDate);
+          const index = data.findIndex((item) =>
+            isSameDay(new Date(item.date), selectedDate)
+          );
+          const hasData = index !== -1;
 
-          // 데이터 확인 결과에 따라 상태 업데이트
+          if (hasData) {
+            setText(data[index].text);
+            setImageUrl(data[index].imageFile);
+          }
+
           setSelectedDateHasData(hasData);
         } catch (error) {
           console.error("Error fetching data:", error);
-          // 에러가 발생하면 데이터가 없는 것으로 간주
           setSelectedDateHasData(false);
         }
       }
     };
 
-    fetchDataAndUpdateState();
-  }, [selectedDate, handleRemove]);
+    if (selectedDate) {
+      fetchDataAndUpdateState();
+    }
+  }, [selectedDate]);
 
   return (
     <Background>
       <Body>
-          <LeftBox leftBoxWidth={leftBoxWidth}>
-            <SideBar isOpen={isOpen} />
-          </LeftBox>
+        <LeftBox leftBoxWidth={leftBoxWidth}>
+          <SideBar isOpen={isOpen} />
+        </LeftBox>
         <CalendarBox>
           <MiddleBox middleBoxWidth={middleBoxWidth}>
             <CalendarProvider>
@@ -362,30 +392,29 @@ function CalendarPage() {
               <GrFormPreviousLink />
             </PrevBtn>
             <ResultBox>
-              {" "}
-              {selectedDate &&
-                (selectedDateHasData ? (
-                  <TrueComponentBox>
-                    <TopBox>
-                      <RemoveBtn onClick={handleRemove}>삭제</RemoveBtn>
-                      <DateBox>
-                        {isSelectedMonth}월{isSelectedDay}일
-                      </DateBox>
-                      <EditBtn onClick={handleEdit}>편집</EditBtn>
-                    </TopBox>
-                    <TrueComponentMidBox><ImageBox src={imageUrl} /></TrueComponentMidBox>
-                    <Divider />
-                    <BottomBox>{text}</BottomBox>
-                  </TrueComponentBox>
-
-                ) : (
-                  <FalseComponent
-                    currentYear={selectedDate.getFullYear()}
-                    month={selectedDate.getMonth() + 1}
-                    day={selectedDate.getDate()}
-                    selectedDate={selectedDate}
-                  />
-                ))}
+              {selectedDate && selectedDateHasData ? (
+                <TrueComponentBox>
+                  <TopBox>
+                    <RemoveBtn onClick={handleRemove}>삭제</RemoveBtn>
+                    <DateBox>
+                      {isSelectedMonth}월{isSelectedDay}일
+                    </DateBox>
+                    <EditBtn onClick={handleEdit}>편집</EditBtn>
+                  </TopBox>
+                  <TrueComponentMidBox>
+                    <ImageBox src={imageUrl} />
+                  </TrueComponentMidBox>
+                  <Divider />
+                  <BottomBox>{text}</BottomBox>
+                </TrueComponentBox>
+              ) : selectedDate ? (
+                <FalseComponent
+                  currentYear={selectedDate.getFullYear()}
+                  month={selectedDate.getMonth() + 1}
+                  day={selectedDate.getDate()}
+                  selectedDate={selectedDate}
+                />
+              ) : null}{" "}
             </ResultBox>
           </RightBox>
         </CalendarBox>
