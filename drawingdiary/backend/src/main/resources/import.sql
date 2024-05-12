ce833f4a1f955760ea73b5da8de2dbaa33806bee
gawon <gawonlee0712@gmail.com>
2024-05-12 16:39:41 +0900

    [가원] 스타일 중복 수정

--- drawingdiary/backend/src/main/resources/import.sql
+++ drawingdiary/backend/src/main/resources/import.sql
@@ -1,8 +1,7 @@
 # 테스트용 자동설정 쿼리, 사용하지 않으나 참고용으로 남겨둠
 # 사용하려면 ddl-auto : create나 create-drop으로 변경하기 근데 데이터 전체 초기화되니까 주의
 
-    
-    INSERT INTO ImageStyle (styleName) VALUES
+INSERT INTO ImageStyle (styleName) VALUES
     ('미니멀'),
     ('뮤지컬'),
     ('전쟁'),
@@ -25,13 +24,12 @@
     ('슈퍼 히어로'),
     ('흑백'),
     ('다큐멘터리'),
-    ('뮤지컬'),
     ('서부'),
     ('스포츠'),
     ('애니메이션'),
     ('드라마'),
     ('SF'),
-    ('역사'),
     ('모험');
+
 
 
