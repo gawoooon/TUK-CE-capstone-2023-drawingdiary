46b7a4a45f8329bef760acdca5e0ee64ebeebd90
jiwon <jiwon0768@tukorea.ac.kr>
2024-05-14 01:22:35 +0900

    [지원] 토큰 재검토 필요함

--- drawingdiary/frontend/src/auth/context/AuthContext.jsx
+++ drawingdiary/frontend/src/auth/context/AuthContext.jsx
@@ -12,30 +12,28 @@ export const AuthProvider = ({ children }) => {
   });
 
   const login = (accessToken, refreshToken, memberID) => {
-    Cookies.set('accessToken', accessToken, { expires : 1/48, path: '/' });
-    Cookies.set('refreshToken', refreshToken, { expires : 2, path: '/' });
-    Cookies.set('memberID', memberID, { expires : 2, path: '/' });
+    Cookies.set('accessToken', accessToken, { path: '/' });
+    Cookies.set('refreshToken', refreshToken, { path: '/' });
+    Cookies.set('memberID', memberID, { path: '/' });
     setAuth({ accessToken: accessToken, refreshToken: refreshToken, memberID: memberID });
   };
 
   const refreshToken = async () => {
     try {
-      const res = await axios.get('http://localhost:8080/api/refresh', {
-        headers: {
-          'Authorization' : `Bearer: ${auth.accessToken}`
-        }
-      });
+      const res = await axios.get('http://localhost:8080/api/refresh');
       const { accessToken } = res;
-      Cookies.set('accessToken', accessToken, { expires: 1/48});
+      Cookies.set('accessToken', accessToken, { path: '/'});
       setAuth(prev => ({ ...prev, accessToken }));
+      return accessToken;
     } catch(error) {
+      // refresh token도 만료되면 다시 로그인.
       console.log("Error refreshing token: ", error);
+      logout();
     }
   }
 
-  const getToken = () => {
-    const token = Cookies.get("accessToken");
-    return token;
+  const getToken = () => {;
+    return auth.accessToken;
   }
 
   const logout = () => {
@@ -47,13 +45,24 @@ export const AuthProvider = ({ children }) => {
   };
   
   useEffect(() => {
-    const interval = setInterval(() => {
-      if(auth.accessToken && auth.refreshToken) {
-        localStorage.clear();
-        refreshToken();
+    const interceptor = axios.interceptors.response.use(
+      response => response,
+      async (error) => {
+        if(error.response.status === 401) {
+          alert("로그인이 만료되었습니다.");
+          localStorage.clear();
+          const newAccessToken = await refreshToken();
+          console.log("newAccessToken: ", newAccessToken);
+          console.log("newToken : ");
+          error.config.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return axios(error.config);
+        }
+        return Promise.reject(error);
       }
-    }, 1000 * 60 * 28);
-    return () => clearInterval(interval);
+    )
+    return () => {
+      axios.interceptors.response.eject(interceptor);
+    };
   }, [auth.accessToken, auth.refreshToken]);
 
   return (
