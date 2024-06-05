2da5a5d6303074bda3adf01c234ce1f367825e7d
jiwon <jiwon0768@tukorea.ac.kr>
2024-06-05 23:30:24 +0900

    [fix] 사이드바 수정

--- drawingdiary/frontend/src/components/sidebar/NavBar.jsx
+++ drawingdiary/frontend/src/components/sidebar/NavBar.jsx
@@ -10,23 +10,23 @@ import styled from "styled-components";
 import { useAuth } from "../../auth/context/AuthContext";
 
 const SideBarStyle = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  width: 16%;
-  height: 100vh;
-  background-color: #eeeeee;
-  padding: 0 6px;
-  z-index: 9999;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 260px;
+    height: 100vh;
+    background-color: #eeeeee;
+    padding: 0 6px;
+    z-index: 9999;
 `;
 
 const HeaderSection = styled.div`
-  margin-top: 45px;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 165px;
+    margin-top: 45px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 165px;
 `;
 
 const MenuItem = styled(Link)`
@@ -44,17 +44,31 @@ const MenuItem = styled(Link)`
 `;
 
 const MenuItemText = styled.div`
-    margin-left: 20px;
+    margin-left: 10px;
     color: #0d0d0d;
     font-size: 1rem;
 `;
 
+const CalenderSection = styled.section`
+    margin-top: 70px;
+    width: 100%;
+    height: 236px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const CalendarStyle = styled.div`
+    width: 100%;
+    height: 100%;
+`;
+
 const RecentSection = styled.section`
-  margin-top: 70px;
-  width: 100%;
-  height: 174px;
-  display: flex;
-  flex-direction: column;
+    margin-top: 70px;
+    width: 100%;
+    height: 174px;
+    display: flex;
+    flex-direction: column;
 `;
 
 const RecentList = styled.div`
@@ -84,46 +98,32 @@ const ProfileSection = styled(Link)`
 `;
 
 const ProfileImg = styled.img`
-  width: 48px;
-  height: 48px;
-  margin-left: 8px;
-  border-radius: 50%;
-  object-fit: cover;
+    width: 48px;
+    height: 48px;
+    margin-left: 8px;
+    border-radius: 50%;
+    object-fit: cover;
 `;
 
 const ProfileName = styled.div`
-    margin-left: 20px;
+    margin-left: 10px;
     font-size: 1rem;
     color: #0d0d0d;
 `;
 
-const Navbar = () => {
+const Navbar = ({ isOpen }) => {
 
-  const { logout, getToken } = useAuth();
-  const accessToken = getToken();
+    const [loginState, setLoginState] = useState(false);
 
-  const setProfileImg = localStorage.getItem("setProfileImage");
+    const { logout, getToken } = useAuth();
+    const accessToken = getToken();
 
-  const diaryList = [
-    "2024년 1월 1일",
-    "2024년 1월 1일",
-    "2024년 1월 1일",
-    "2024년 1월 1일",
-    "2024년 1월 1일",
-  ];
+    const setProfileImg = localStorage.getItem("setProfileImage");
 
-  useEffect(() => {
-    const currentAccessToken = getToken();
-    if (currentAccessToken) {
-      setLoginState(true);
-    } else {
-      setLoginState(false);
-    }
-  }, [accessToken]);
+    const diaryList = ["2024년 1월 1일", "2024년 1월 1일", "2024년 1월 1일", "2024년 1월 1일", "2024년 1월 1일"];
 
     useEffect(() => {
         const currentAccessToken = getToken();
-        console.log(currentAccessToken);
         if (currentAccessToken) {
         setLoginState(true);
         } else {
@@ -147,68 +147,34 @@ const Navbar = () => {
         }
     };
 
-  return (
-    <SideBarStyle>
-      <HeaderSection>
-        <MenuItem to="/">
-          <LuCalendarDays
-            size={20}
-            color="#3d3d3d"
-            alt="Album"
-            style={{ margin: "1px 0" }}
-          />
-          <MenuItemText>캘린더</MenuItemText>
-        </MenuItem>
-        <MenuItem to="/album">
-          <BiSolidPhotoAlbum
-            size={20}
-            color="#3d3d3d"
-            alt="Album"
-            style={{ margin: "1px 0" }}
-          />
-          <MenuItemText>앨범</MenuItemText>
-        </MenuItem>
-        <MenuItem to="/stats">
-          <SlGraph
-            size={20}
-            color="#3d3d3d"
-            alt="Statics"
-            style={{ margin: "1px 0" }}
-          />
-          <MenuItemText>분석</MenuItemText>
-        </MenuItem>
-        {loginState ? (
-          <MenuItem to="/login" onClick={handleLogout}>
-            <IoMdLogOut
-              size={20}
-              color="#3d3d3d"
-              alt="Logout"
-              style={{ margin: "1px 0" }}
-            />
-            <MenuItemText>로그아웃</MenuItemText>
-          </MenuItem>
-        ) : (
-          <MenuItem to="/login">
-            <IoMdLogIn
-              size={20}
-              color="#3d3d3d"
-              alt="Login"
-              style={{ margin: "1px 0" }}
-            />
-            <MenuItemText>로그인</MenuItemText>
-          </MenuItem>
-        )}
-      </HeaderSection>
 
-      <RecentSection>
-        {diaryList.length > 0 ? (
-          diaryList.map((date, index) => (
-            <RecentList key={index}>{date}</RecentList>
-          ))
-        ) : (
-          <RecentList>no recent data.</RecentList>
-        )}
-      </RecentSection>
+    return (
+        <SideBarStyle>
+        <HeaderSection>
+            <MenuItem to="/">
+            <LuCalendarDays size={20} color="#3d3d3d" alt="Album" style={{margin: '1px 0'}}/>
+            <MenuItemText>캘린더</MenuItemText>
+            </MenuItem>
+            <MenuItem to="/album">
+            <BiSolidPhotoAlbum size={20} color="#3d3d3d" alt="Album" style={{margin: '1px 0'}}/>
+            <MenuItemText>앨범</MenuItemText>
+            </MenuItem>
+            <MenuItem to="/stats">
+            <SlGraph size={20} color="#3d3d3d" alt="Statics" style={{margin: '1px 0'}}/>
+            <MenuItemText>분석</MenuItemText>
+            </MenuItem>
+            {loginState ? (
+            <MenuItem to="/login" onClick={handleLogout}>
+                <IoMdLogOut size={20} color="#3d3d3d" alt="Logout" style={{margin: '1px 0'}}/>
+                <MenuItemText>로그아웃</MenuItemText>
+            </MenuItem>
+            ) : (
+            <MenuItem to="/login">
+                <IoMdLogIn size={20} color="#3d3d3d" alt="Login" style={{margin: '1px 0'}}/>
+                <MenuItemText>로그인</MenuItemText>
+            </MenuItem>
+            )}
+        </HeaderSection>
 
         <RecentSection>
             {diaryList.length > 0 ? (
